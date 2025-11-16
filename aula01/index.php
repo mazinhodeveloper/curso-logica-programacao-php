2ff0310fd77035bee7e2765e2a920b66a251e4da
Mazinho Developer <mazinhodeveloper@gmail.com>
2025-11-16 20:55:37 -0300

    Adicionando assets e aula01 - index.php

---
+++ aula01/index.php
@@ -0,0 +1,31 @@
+<?php require_once "views/header.php"; ?>
+      
+      <main>
+        <div class="d-flex mb-4 align-items-center font-size-14">
+          <div class="vr"></div>
+          <p class="ms-2 margin-bottom-0 font-size-14">
+            Aula 01<br>
+            Docente: Celso Luis Caldeira<br>
+            Data: 17/11/2025
+          </p>
+        </div>
+        
+        <h1>Lógica de Programação Direcionada a PHP - 40 horas</h1> 
+        <p class="mb-4">
+          Aprender a interpretar a sintaxe e o formato da linguagem de programação PHP para criar algoritmos e desenvolver aplicações e sistemas computacionais web.
+        </p>
+        <p class="margin-bottom-0 font-size-14">
+          Período: 17/11/2025 a 10/12/2025<br>
+          Horário: Segunda, Quarta e Sexta - 08h às 12h
+        </p>
+        <p class="font-size-14 mb-4">
+          <a href="https://www.sp.senac.br/cursos-livres/curso-de-logica-de-programacao-direcionada-a-php" target="_blank">https://sp.senac.br/cursos-livres/curso-de-logica-de-programacao-direcionada-a-php</a>
+        </p>
+        
+        <h2>GitHub / Lógica de Programação Direcionada a PHP</h2>
+        <p class="mb-4">
+          <a href="https://github.com/mazinhodeveloper/curso-logica-programacao-php" target="_blank">https://github.com/mazinhodeveloper/curso-logica-programacao-php</a>
+        </p>
+      </main>
+      
+  <?php require_once "views/footer.php"; ?>
