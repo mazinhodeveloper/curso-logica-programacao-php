6bf32b7774126a84b81de17b6607b8067d59b42a
Mazinho Developer <mazinhodeveloper@gmail.com>
2025-11-28 08:18:19 -0300

    Atualizando aula04

--- aula04/financeiro/login.php
+++ aula04/financeiro/login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start(); // Inicia uma seção
 // Quando se inicia uma seção estamos preparando
@@ -39,48 +38,4 @@ try {
 } catch (PDOException $e) {
     echo "Erro: " . $e->getMessage();
 }
-
-=======
-<?php
-session_start(); // Inicia uma seção
-// Quando se inicia uma seção estamos preparando
-// para enviar alguns parametros para outra página
-// Vamos enviar o ID, o email para o principal.php
-// Dizendo então que se está logado. "Fator de Segurança"
-include 'conexao.php';
-$usuario = $_POST['email'];
-$senha = $_POST['senha'];
-try {
-    // Preparar a consulta SQL
-    $sql = "SELECT * FROM usuarios where email = :email";
-    $stmt = $pdo->prepare($sql);
-    // Vincular os parametros corretamente
-    $stmt->bindParam(':email', $usuario, PDO::PARAM_STR);
-    // Executar a consulta
-    $stmt->execute();
-    // Trouxe todos os campos do usuário para uma lista (array)
-    $usuarioEncontrado = $stmt->fetch(PDO::FETCH_ASSOC);
-    if ($usuarioEncontrado) {
-        $id_usuario = $usuarioEncontrado['id_usuario'];
-        $hashed_password = $usuarioEncontrado['senha'];
-        $nome_usuario = $usuarioEncontrado['nome'];
-        //Verifica se a senha está correta
-        if (password_verify($senha, $hashed_password)) {
-            //Armazenar dados da seção
-            $_SESSION['id_usuario'] = $id_usuario;
-            $_SESSION['nome_usuario'] = $nome_usuario;
-            // Redireciona para o proncipal.php;
-            header('location:principal.php');
-            exit();
-        }
-
-    }
-    // Se a senha for invalida ou usuário não encontrado
-    // Criar um php chamado usuario_recusado.php
-    exit();
-} catch (PDOException $e) {
-    echo "Erro: " . $e->getMessage();
-}
-
->>>>>>> a8d0542b7829f62e467f55fa2aa34013dcf8786c
 ?>
--- aula04/financeiro/principal.php
+++ aula04/financeiro/principal.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -18,31 +17,5 @@
      </div>
 
 
-
-
 </body>
-=======
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema Financeiro</title>
-
-</head>
-<body>
-    <?php
-    include 'menu.php';
-     ?>
-     <div class="container">
-        <?php
-            echo "<h2 style='text-align: center';>Bem Vindo, $nome_usuario! </h2>";
-        ?>
-     </div>
-
-
-
-
-</body>
->>>>>>> a8d0542b7829f62e467f55fa2aa34013dcf8786c
 </html>
