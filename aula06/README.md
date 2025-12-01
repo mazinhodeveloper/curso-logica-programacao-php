be5629eb4113a65e744710d40152734802a95778
Mazinho Developer <mazinhodeveloper@gmail.com>
2025-12-01 08:14:05 -0300

    Adicionando aula06

---
+++ aula06/README.md
@@ -0,0 +1,10 @@
+> Aula 06   
+> Docente: Celso Luis Caldeira    
+> Data: 01/12/2025   
+
+# GitHub / Curso Lógica de Programação Direcionada a PHP 
+https://github.com/mazinhodeveloper/curso-logica-programacao-php   
+
+## Atividades 
+Atividades desenvolvidas em sala de aula.   
+- financeiro
---
+++ aula06/index.php
@@ -0,0 +1,51 @@
+<?php require_once "views/header.php"; ?>
+      
+      <main>
+        <div class="d-flex mb-4 align-items-center font-size-14">
+          <div class="vr"></div>
+          <p class="ms-2 margin-bottom-0 font-size-14">
+            Aula 05<br>
+            Docente: Celso Luis Caldeira<br>
+            Data: 01/12/2025
+          </p>
+        </div>
+        
+        <h1>Lógica de Programação Direcionada a PHP - 40 horas</h1> 
+        <p class="mb-4 margin-bottom-0">
+          Aprender a interpretar a sintaxe e o formato da linguagem de programação PHP para criar algoritmos e desenvolver aplicações e sistemas computacionais web.
+        </p>
+        <p class="margin-bottom-0 font-size-14">
+          Período: 17/11/2025 a 10/12/2025<br>
+          Horário: Segunda, Quarta e Sexta - 08h às 12h
+        </p>
+        <p class="font-size-14 mb-4">
+          <a href="https://www.sp.senac.br/cursos-livres/curso-de-logica-de-programacao-direcionada-a-php" target="_blank">https://sp.senac.br/cursos-livres/curso-de-logica-de-programacao-direcionada-a-php</a>
+        </p>
+        
+        <h2>GitHub / Lógica de Programação Direcionada a PHP</h2>
+        <p class="mb-4">
+          <a href="https://github.com/mazinhodeveloper/curso-logica-programacao-php" target="_blank">https://github.com/mazinhodeveloper/curso-logica-programacao-php</a>
+        </p>
+
+        <h3>Atividades</h3> 
+        <p class="mb-4 margin-bottom-0">Atividades desenvolvidas em sala de aula.</p> 
+        <p class="mb-4">
+          -  
+        </p> 
+
+        <h4>Utilizades</h4>
+        <p>
+          - https://getbootstrap.com<br>
+<<<<<<< HEAD
+          - https://datatables.net/examples/styling/bootstrap5 
+=======
+          - https://getbootstrap.com/docs/5.3/getting-started/introduction/<br>
+          - https://getbootstrap.com/docs/5.3/components/navbar/<br>
+          - https://getbootstrap.com/docs/5.3/forms/overview/<br>
+          - https://getbootstrap.com/docs/5.3/content/tables/<br>
+          - https://getbootstrap.com/docs/5.3/layout/grid/
+>>>>>>> a8d0542b7829f62e467f55fa2aa34013dcf8786c
+        </p>
+      </main>
+      
+  <?php require_once "views/footer.php"; ?>
